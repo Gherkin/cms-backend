cb8279625ad77985bcf72165cbbcb4e242538406
Gherkin <niyohn@stekligan.se>
2015-10-30 23:57:45 +0100

    fixed NPE by constructor injection

--- src/main/java/com/github/gherkin/ChangeLog.java
+++ src/main/java/com/github/gherkin/ChangeLog.java
@@ -6,9 +6,11 @@ import com.google.inject.Inject;
 import java.util.ArrayList;
 
 public class ChangeLog extends ArrayList<String> {
-    @Inject
     ChangeLogDao dao;
-    public ChangeLog() {
+
+    @Inject
+    public ChangeLog(ChangeLogDao dao) {
+        this.dao = dao;
         this.addAll(dao.retrieveAll());
     }
 
